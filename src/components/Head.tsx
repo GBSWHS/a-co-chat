727650ecb0ad55ab6737a1d9dd21c26392b08e6e
cth.release <84012697+th-release@users.noreply.github.com>
2023-03-06 12:05:38 +0900

    feat: Create Head.tsx

---
+++ src/components/Head.tsx
@@ -0,0 +1,12 @@
+import NextHead from 'next/head'
+
+export default (() => {
+  return (
+    <NextHead>
+      <title>아코!</title>
+      <meta name="description" content="경소고의 질문은 모두 아코가 해결해줄게!" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </NextHead>
+  )
+})
